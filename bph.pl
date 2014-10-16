5cea14dab20bdf570782b1534105a4980ffe0145
Diamantis Sellis <sellisd@gmail.com>
2014-10-16 15:23:04 +0200

    fix input output name correspondences for bph.pl. Wait for all forked processes to finish before proceeding the downstream analysis

--- bph.pl
+++ bph.pl
@@ -8,11 +8,12 @@ use Parallel::ForkManager;
 #cat p1.fa s2.fa > combined.fa
 
 #make database for all proteins
-#  makeblastdb -in combined.fa -dbtype prot
+#  makeblastdb -in pb.fa -dbtype prot -out combined
 
 # for some reason cat messed up some new lines so watch the output of makeblsatdb
 
-my $pm = Parallel::ForkManager->new(8);
+my $cores  = 8;
+my $pm = Parallel::ForkManager->new($cores);
 # pipeline for analysis of Paremecium data
 
 #set data paths
@@ -21,6 +22,8 @@ my $fastaInPath = 'working/';
 my $fastaOutPath = 'working/';
 my $blastOutPath = 'working/';
 
+my @tempFiles; #fill array with temporary files to be deleted
+
 #blast all against all but keep all hits and output in tab file
 #  blastp -query combined.fa -db combined.fa -outfmt 6
 #or ?faster? read fasta file one sequence at a time and blast it against the database
@@ -28,37 +31,44 @@ my $file = "/Users/diamantis/data/IES_data/working/combined.fa";
 my $seqF = Bio::SeqIO->new('-file'=>$file,
 			   '-format'=>'fasta');
 #split fasta file in chunkcs
-my $batch = 10;
+my $batch = 100;
 my $counter = 0;
 my $fileCounter = 0;
-my $seqOut = Bio::SeqIO->new('-file'=>'>'.$fastaOutPath.'chunk.'.$fileCounter.'.fa',
+my $fastaOutFile = $fastaOutPath.'chunk.'.$fileCounter.'.fa';
+my $seqOut = Bio::SeqIO->new('-file'=>'>'.$fastaOutFile,
 			     '-format'=>'fasta');
+push @tempFiles, $seqOut;
 print "splitting fasta file in chunks of $batch sequences\n";
 while(my $seqO = $seqF->next_seq){
     print "   $counter\r";
     if ($counter % $batch == 0){
-	$seqOut = Bio::SeqIO->new('-file'=>'>'.$fastaOutPath.'chunk.'.$fileCounter.'.fa',
+	$fastaOutFile = $fastaOutPath.'chunk.'.$fileCounter.'.fa';
+	$seqOut = Bio::SeqIO->new('-file'=>'>'.$fastaOutFile,
 				  '-format'=>'fasta');
+	push @tempFiles, $fastaOutFile;
 	$fileCounter++;
     }
     $seqOut->write_seq($seqO);
     $counter++;
 }
 print "   $counter sequences in $fileCounter pieces\n";
-print "   done\n";
+print "     done\n";
 
 print "running blast\n";
 for(my $i = 0 ; $i<$fileCounter; $i++){
     print "   $i/$fileCounter\r";
     my $pid = $pm->start and next;
-    my $cmdString = 'blastp -query '.$fastaOutPath.'chunk.'.$i.'.fa -db '.$fastaInPath.'combined.fa -outfmt 6';
+    my $cmdString = 'blastp -query '.$fastaOutPath.'chunk.'.$i.'.fa -db '.$fastaInPath.'combined -outfmt 6';
     my $result = `$cmdString`;
-    open OUT, '>'.$blastOutPath.'blastout.'.$i.'.dat' or die $!;
+    my $blastOutFile = $blastOutPath.'blastout.'.$i.'.dat';
+    open OUT, '>'.$blastOutFile or die $!;
     print OUT $result;
     close OUT;
+    push @tempFiles, $blastOutFile;
     $pm->finish;
 }
-print "   done\n";
+$pm->wait_all_children;
+print "     done\n";
 
 print "stitch output\n";
 my $cmdString = 'cat '.$blastOutPath.'blastout.*.dat > '.$blastOutPath.'4silix.dat';
@@ -70,3 +80,5 @@ system($cmdString);
 #silix fastafile blastfile > silix.output
 
 #delete intermidiate files
+print "deleting temp files\n";
+unlink @tempFiles or die $!;
