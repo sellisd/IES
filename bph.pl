e20076eca456ab6d10d51989e737457c5a079129
Diamantis Sellis <sellisd@gmail.com>
2014-10-16 14:53:50 +0200

    bph.pl splits a fasta file in chunks and runs blast on each one in a separate core, then concatenates the output ready for silix

--- bph.pl
+++ bph.pl
@@ -2,8 +2,7 @@
 use warnings;
 use strict;
 use Bio::SeqIO;
-# pipeline for analysis of Paremecium data
-
+use Parallel::ForkManager;
 #make sure each protein has unique ID including in which species it belongs
 #concatenate all fasta files to create on big one
 #cat p1.fa s2.fa > combined.fa
@@ -13,6 +12,15 @@ use Bio::SeqIO;
 
 # for some reason cat messed up some new lines so watch the output of makeblsatdb
 
+my $pm = Parallel::ForkManager->new(8);
+# pipeline for analysis of Paremecium data
+
+#set data paths
+my $blastdbPath = 'working/';
+my $fastaInPath = 'working/';
+my $fastaOutPath = 'working/';
+my $blastOutPath = 'working/';
+
 #blast all against all but keep all hits and output in tab file
 #  blastp -query combined.fa -db combined.fa -outfmt 6
 #or ?faster? read fasta file one sequence at a time and blast it against the database
@@ -20,25 +28,45 @@ my $file = "/Users/diamantis/data/IES_data/working/combined.fa";
 my $seqF = Bio::SeqIO->new('-file'=>$file,
 			   '-format'=>'fasta');
 #split fasta file in chunkcs
-my $batch = 100;
+my $batch = 10;
 my $counter = 0;
 my $fileCounter = 0;
-my $seqOut = Bio::SeqIO->new('-file'=>'>chunk.'.$fileCounter,
+my $seqOut = Bio::SeqIO->new('-file'=>'>'.$fastaOutPath.'chunk.'.$fileCounter.'.fa',
 			     '-format'=>'fasta');
+print "splitting fasta file in chunks of $batch sequences\n";
 while(my $seqO = $seqF->next_seq){
-  if ($counter % $batch == 0){
-    $seqOut = Bio::SeqIO->new('-file'=>'>chunk.'.$fileCounter,
-			     '-format'=>'fasta');
-    $fileCounter++;
-  }
-  $seqOut->write_seq($seqO);
-  $counter++;
+    print "   $counter\r";
+    if ($counter % $batch == 0){
+	$seqOut = Bio::SeqIO->new('-file'=>'>'.$fastaOutPath.'chunk.'.$fileCounter.'.fa',
+				  '-format'=>'fasta');
+	$fileCounter++;
+    }
+    $seqOut->write_seq($seqO);
+    $counter++;
 }
+print "   $counter sequences in $fileCounter pieces\n";
+print "   done\n";
 
+print "running blast\n";
+for(my $i = 0 ; $i<$fileCounter; $i++){
+    print "   $i/$fileCounter\r";
+    my $pid = $pm->start and next;
+    my $cmdString = 'blastp -query '.$fastaOutPath.'chunk.'.$i.'.fa -db '.$fastaInPath.'combined.fa -outfmt 6';
+    my $result = `$cmdString`;
+    open OUT, '>'.$blastOutPath.'blastout.'.$i.'.dat' or die $!;
+    print OUT $result;
+    close OUT;
+    $pm->finish;
+}
+print "   done\n";
+
+print "stitch output\n";
+my $cmdString = 'cat '.$blastOutPath.'blastout.*.dat > '.$blastOutPath.'4silix.dat';
+system($cmdString);
 #blast one chunk at a time (on a different core 8) ) 
-
 #output in tabular format for Silix
 
 # run silix for clustering
 #silix fastafile blastfile > silix.output
 
+#delete intermidiate files
