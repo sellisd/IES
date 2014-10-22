37f034cc0bed91c97255c28646d14449606d147f
Diamantis Sellis <sellisd@gmail.com>
2014-10-22 11:06:07 +0200

    makeBed.pl fully functional

--- makeBed.pl
+++ makeBed.pl
@@ -3,6 +3,9 @@ use warnings;
 use strict;
 use Bio::SeqIO;
 use Bio::SeqFeature::Generic;
+
+# make bed files for CDS and IES from genbank file
+
 my $dataPath = '/Users/diamantis/data/IES_data/ptetraurelia/';
 my $inputF = 'PTET.IES.gnbk';
 my $gnbkIn = Bio::SeqIO->new('-file' => $dataPath.$inputF,
@@ -16,27 +19,29 @@ open CDSOUT, '>'.$dataPath.$cdsout or die $!;
 open IESOUT, '>'.$dataPath.$iesout or die $!;
 
 while(my $seqO = $gnbkIn->next_seq()){
+    my $scaffold = $seqO->accession_number();
     foreach my $featureO ($seqO->get_SeqFeatures()){
-#	print $featureO->primary_tag(),"\n";die;
 	if($featureO->primary_tag() eq 'CDS'){
 	    my @geneName = $featureO->get_tag_values('gene');
 	    my $locationO = $featureO->location();
  	    foreach my $locations ($locationO->each_Location()){
  		my $start = $locations->start;
  		my $end = $locations->end;
-		print CDSOUT "$geneName[0] $start $end\n";
+		print CDSOUT "$scaffold\t$start\t$end\t$geneName[0]\n";
 	    }
 	}
 	if($featureO->primary_tag() eq 'IES_junction'){
 	    my $locationO = $featureO->location();
+	    # print $featureO->get_all_tags();
+	    # die;
 	    my @id = $featureO->get_tag_values('id');
 	    my $start = $locationO->start;
 	    my $end = $locationO->end;
-	    print IESOUT "$start $end\n";
+	    print IESOUT "$scaffold\t$start\t$end\t$id[0]\n";
 	}
     }
 }
 		
-# make bed files for CDS and IES from genbank file
+
 close CDSOUT;
 close IESOUT;
