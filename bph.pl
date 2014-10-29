657b428761df7c2ebbdb3fe38018539b5bf8633a
Diamantis Sellis <sellisd@gmail.com>
2014-10-29 17:13:41 +0100

    bph.pl adjusted for running on the virtual machine

--- bph.pl
+++ bph.pl
@@ -6,32 +6,27 @@ use Parallel::ForkManager;
 
 #script runs all-against-all blast of proteins after filtering low complexity regions and performs silix grouping
 
-#make sure each protein has unique ID including in which species it belongs
-#concatenate all fasta files to create on big one
-#cat pbiaurelia/biaurelia_V1-4_annotation_v1.protein.fa psexaurelia/sexaurelia_AZ8-4_annotation_v1.protein.fa ptetraurelia/ptetraurelia_mac_51_annotation_v2.0.4.protein.fa > combined.fa
 
-#make database for all proteins
-#  makeblastdb -in pb.fa -dbtype prot -out combined
-
-# for some reason cat messed up some new lines so watch the output of makeblsatdb
+my $file = "combined.fa"; #fasta file
+my $blastpBin = 'blastp';
+my $silixBin = 'silix';
 
 my $cores  = 8;
 my $pm = Parallel::ForkManager->new($cores);
 # pipeline for analysis of Paremecium data
 
 #set data paths
-my $blastdbPath = 'working/';
-my $fastaInPath = 'working/';
+my $blastdbPath = './';
 my $fastaOutPath = 'working/';
 my $blastOutPath = 'working/';
-
+my $silixOutPath = 'working/';
 my @tempFiles; #fill array with temporary files to be deleted
 
 #blast all against all but keep all hits and output in tab file
 #  blastp -query combined.fa -db combined.fa -outfmt 6
 #or ?faster? read fasta file one sequence at a time and blast it against the database
 
-my $file = "/Users/diamantis/data/IES_data/working/combined.fa";
+
 my $seqF = Bio::SeqIO->new('-file'=>$file,
 			   '-format'=>'fasta');
 #split fasta file in chunkcs
@@ -62,8 +57,7 @@ print "running blast\n";
 for(my $i = 0 ; $i<$fileCounter; $i++){
     print "   $i/$fileCounter\r";
     my $pid = $pm->start and next;
-    my $cmdString = 'blastp -query '.$fastaOutPath.'chunk.'.$i.'.fa -db '.$fastaInPath.'combined -outfmt 6';
-#    my $cmdString = '/home/dsellis/tools/ncbi-blast-2.2.29+/bin/blastp -query '.$fastaOutPath.'chunk.'.$i.'.fa -db '.$fastaInPath.'combined -outfmt 6';
+    my $cmdString = $blastpBin.' -query '.$fastaOutPath.'chunk.'.$i.'.fa -db '.$blastdbPath.'combined -outfmt 6';
     my $result = `$cmdString`;
     my $blastOutFile = $blastOutPath.'blastout.'.$i.'.dat';
     open OUT, '>'.$blastOutFile or die $!;
@@ -83,7 +77,8 @@ system($cmdString);
 
 # run silix for clustering
 #silix fastafile blastfile > silix.output
-#silix working/combined.fa working/4silix.dat > working/silix.output
+my $cmdl = $silixBin.' '.$file.' '.$blastOutPath.'4silix.dat > '.$silixOutPath.'silix.output';
+system $cmdl;
 #
 #delete intermidiate files
 print "deleting temp files\n";
