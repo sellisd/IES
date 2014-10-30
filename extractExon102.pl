ecc98496143a5170e0f0fe501e96f436e6b02d35
Diamantis Sellis <sellisd@gmail.com>
2014-10-30 11:20:03 +0100

    output is split in 3 files, one with dna fasta, one with protein fasta and one tab delimited with name, start, end, length and frame

--- extractExon102.pl
+++ extractExon102.pl
@@ -3,17 +3,24 @@ use warnings;
 use strict;
 use Bio::SeqIO;
 my @species = qw/Pbi Pte Pse/;
-my $OutF = Bio::SeqIO->new('-file' => '>/Users/diamantis/data/IES_data/working/exon102.fa',
+my $PF = Bio::SeqIO->new('-file' => '>/Users/diamantis/data/IES_data/working/exon102P.fa',
 			   '-format' => 'fasta');
+my $NF = Bio::SeqIO->new('-file' => '>/Users/diamantis/data/IES_data/working/exon102N.fa',
+			   '-format' => 'fasta');
+my $tabOutF = '>/Users/diamantis/data/IES_data/working/exon102.tab';
 
+#find the 102 exons and print a summary file with
+#id start stop length coding frame
+#and two fasta files one with nucleotide and the aminoacid sequence
+open TO, $tabOutF or die $!;
 foreach my $species (@species){
     my $gene;
     if($species eq 'Pbi'){
-	$gene = '/Users/diamantis/data/IES_data/pbiaurelia/PBI.gnbk';
+	$gene = '/Users/diamantis/data/IES_data/pbiaurelia/Pbi.gnbk';
     }elsif($species eq 'Pte'){
-	$gene = '/Users/diamantis/data/IES_data/ptetraurelia/PTET.gnbk';
+	$gene = '/Users/diamantis/data/IES_data/ptetraurelia/Pte.gnbk';
     }elsif($species eq 'Pse'){
-	$gene = '/Users/diamantis/data/IES_data/psexaurelia/PSEX.gnbk';
+	$gene = '/Users/diamantis/data/IES_data/psexaurelia/Pse.gnbk';
     }else{
 	die;
     }
@@ -29,13 +36,25 @@ foreach my $species (@species){
 		my $start = $featureO->start();
 		my $end = $featureO->end();
 		my $gene = ($featureO->get_tag_values('gene'))[0];
+		my $codon_start = ($featureO->get_tag_values('codon_start'))[0];
+		my $id = 'P_'.$species->species().'_'.$seqO->accession_number().'_'.$gene.'_'.$start.'_'.$end;
 		if($length == 102){
-		    my $newSeq = Bio::Seq->new('-seq' => $seqO->subseq($start,$end),
-					       '-id' => 'P_'.$species->species().'_'.$seqO->accession_number().'_'.$gene.'_'.$start.'_'.$end,
+		    my $newNSeq = Bio::Seq->new('-seq' => $seqO->subseq($start,$end),
+						'-id' => $id,
+						'-alphabet' => 'dna'
 			);
-		    $OutF->write_seq($newSeq);
+		    my $translation = $newSeq->translate(-frame=>$codon_start,
+							 -codontable_id = > 6);
+		    my $newPSeq = Bio::Seq->new('-seq' => $translation,
+						'-id' => $id,
+						'-alphabet' => 'protein'
+			);
+		    $PF->write_seq($newPSeq);
+		    $NF->write_seq($newNSeq);
+		    print TO "$id\t$start\t$stop\t$length\t$codon_start\n";
 		}
 	    }
 	}
     }
 }
+close TO;
