4d2effe527afa436dcbf9aeed4166f6879cb6bbd
Diamantis Sellis <sellisd@gmail.com>
2014-11-06 16:24:30 +0100

    split bph.pl in two. preblast.pl prepares the files for blast by splitting the fasta file in chunks which have to be uploaded to each node of the cluter. then the new bph.pl only runs all-against-all blast

---
+++ preblast.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Bio::SeqIO;
+my $file = "/Users/diamantis/data/IES_data/working/combined.fa"; #fasta file
+my $fastaOutPath = '/Users/diamantis/data/IES_data/working/fasta/';
+mkdir $fastaOutPath unless -d $fastaOutPath;
+#prepare fasta files for running blast on cluster
+my $seqF = Bio::SeqIO->new('-file'=>$file,
+			   '-format'=>'fasta');
+#split fasta file in chunkcs
+my $batch = 100;
+my $counter = 0;
+my $fileCounter = 0;
+my $fastaOutFile = $fastaOutPath.'chunk.'.$fileCounter.'.fa';
+my $seqOut = Bio::SeqIO->new('-file'=>'>'.$fastaOutFile,
+			     '-format'=>'fasta');
+
+print "splitting fasta file in chunks of $batch sequences\n";
+while(my $seqO = $seqF->next_seq){
+    print "   $counter\r";
+    if ($counter % $batch == 0){
+	$fastaOutFile = $fastaOutPath.'chunk.'.$fileCounter.'.fa';
+	$seqOut = Bio::SeqIO->new('-file'=>'>'.$fastaOutFile,
+				  '-format'=>'fasta');
+	$fileCounter++;
+    }
+    $seqOut->write_seq($seqO);
+    $counter++;
+}
+
+print "   $counter sequences in $fileCounter pieces\n";
+print "     done\n";
