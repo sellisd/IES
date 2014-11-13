76d5ca6d2145d3242b7427016375196ad6ad8e86
Diamantis Sellis <sellisd@gmail.com>
2014-11-13 14:01:08 +0100

    splitPBS.pl finds which runs were not completed, splits the PBS files to smaller chunks and reruns them

--- splitPBS.pl
+++ splitPBS.pl
@@ -3,14 +3,15 @@ use warnings;
 use strict;
 #if a pbs is too long for execution in the cluster split the file to smaller pieces and rerun it
 #find which ones to split
-opendir(DH, $ARGV[0]) or die $!;
+my $path = $ARGV[0];
+opendir(DH, $path) or die $!;
 my @files = readdir(DH);
 close DH;
-my $step = 10;
-foreach my $file(@files){
-    next unless -s $file;
+my $step = 2;
+my @files2sub;
+foreach my $file (@files){
+    next unless -s $path.$file;
     next unless $file =~ /error.(\d+)/; #keep only error files
-    print $file;
     my $number = $1;
     open PBS, 'msa.'.$number.'.pbs' or die $!;
     my @head;
@@ -30,13 +31,14 @@ foreach my $file(@files){
     my $counter = 0;
     for(my $i = 0; $i<$#commands; $i+=$step){
 	my $fileName = "msa.".$number.'.'.$counter.".pbs";
-	print $fileName,"\n";
 	open NBPS, '>'.$fileName or die $!;
 	foreach my $header (@head){
 	    if($header =~/#PBS(.+)\.$number/){
-		$header = $header.'.'.$counter;
+		chomp $header;
+		print NBPS $header.'.'.$counter."\n";
+	    }else{
+		print NBPS $header;
 	    }
-	    print NBPS $header;
 	}
 	my $to = ($i+$step>$#commands?$#commands:$i+$step);
 	my @slice = @commands[$i..$to];
@@ -44,7 +46,11 @@ foreach my $file(@files){
 	print NBPS @tail;
 	$counter++;
 	close NBPS;
-	print $fileName,"\n";
-#	system "qsub $fileName";
+#build a list of file names and then run all of the together
+	push @files2sub, $fileName;
     }
+}
+foreach my $sub (@files2sub){
+    print $sub,"\n";
+    system "qsub $sub";
 }
