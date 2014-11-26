328f12ab1ec72abd129d48345667f130709a51c4
Diamantis Sellis <sellisd@gmail.com>
2014-11-26 13:35:03 +0100

    silix2fasta.p extract gene and protein sequence of all genes in group.15 of nucleotide silix clustering

--- silix2fasta.pl
+++ silix2fasta.pl
@@ -3,16 +3,24 @@ use warnings;
 use strict;
 use Bio::SeqIO;
 
-my $silixFile = '/Users/diamantis/data/IES_data/working/102P.silix';
+my $silixFile = '/Users/diamantis/data/IES_data/working/102N.silix';
 my @proteinFiles = qw{
 /Users/diamantis/data/IES_data/pbiaurelia/pbiaurelia_V1-4_annotation_v2.0.protein.fa
 /Users/diamantis/data/IES_data/ptetraurelia/ptetraurelia_mac_51_annotation_v2.0.protein.fa
 /Users/diamantis/data/IES_data/psexaurelia/psexaurelia_AZ8-4_annotation_v2.0.protein.fa
 } ;
 
-my $outputFile = '>/Users/diamantis/data/IES_data/working/genes102P.fa';
-my $groupTarget = '1';
+my @geneFiles = qw{
+/Users/diamantis/data/IES_data/pbiaurelia/pbiaurelia_V1-4_annotation_v2.0.gene.fa
+/Users/diamantis/data/IES_data/ptetraurelia/ptetraurelia_mac_51_annotation_v2.0.gene.fa
+/Users/diamantis/data/IES_data/psexaurelia/psexaurelia_AZ8-4_annotation_v2.0.gene.fa
+} ;
+
+my $outputPFile = '>/Users/diamantis/data/IES_data/working/genes102P.fa';
+my $outputNFile = '>/Users/diamantis/data/IES_data/working/genes102N.fa';
+my $groupTarget = '15';
 my %genes;
+my %proteins; #same data but (?)faster access
 #select a group from a silix clustering file and extract its protein sequence in a fasta file
 open IN, $silixFile or die $!;
 
@@ -21,13 +29,14 @@ while(my $line = <IN>){
     (my $group, my $name) = split " ", $line;
     if ($group eq $groupTarget){
 	$name =~ /P.*(P.*)T(\d+)/; #this depends on the output of extractExon102.pl if I correct it (for shorter output and less redundancy in the names I should adjust the regexp
-	$genes{$1.'P'.$2} = 1;
+	$genes{$1.'G'.$2} = 1;
+	$proteins{$1.'P'.$2} = 1;
     }
 }
 close IN;
 
 #use Data::Dumper;print Dumper %genes;die;
-my $OUT = Bio::SeqIO->new('-file' => $outputFile,
+my $OUTP = Bio::SeqIO->new('-file' => $outputPFile,
 			  '-format' => 'Fasta');
 #read protein file(s)
 foreach my $file (@proteinFiles){
@@ -36,10 +45,26 @@ foreach my $file (@proteinFiles){
     while(my $seq = $PF->next_seq()){
 	my $id = $seq->display_id();
 #	print $id,' ', $seq->display_id();die;
-	if (defined($genes{$id})){
-	    $OUT->write_seq($seq);
+	if (defined($proteins{$id})){
+	    $OUTP->write_seq($seq);
 	}
     }
 }
 
-#loop through sequences and print matching ones
+#read gene file(s)
+
+#use Data::Dumper;print Dumper %genes;die;
+my $OUTN = Bio::SeqIO->new('-file' => $outputNFile,
+			  '-format' => 'Fasta');
+#read gene file(s)
+foreach my $file (@geneFiles){
+    my $NF = Bio::SeqIO->new('-file' => $file,
+			     '-format' => 'Fasta');
+    while(my $seq = $NF->next_seq()){
+	my $id = $seq->display_id();
+#	print $id,' ', $seq->display_id();die;
+	if (defined($genes{$id})){
+	    $OUTN->write_seq($seq);
+	}
+    }
+}
