2dbe02ca1752d6486fc47f7f7cf87e419fdada6e
Diamantis Sellis <sellisd@gmail.com>
2015-02-06 10:41:03 +0100

    fixed bug in Nmat.pl with printing output when multiple IES are present

--- Nmat.pl
+++ Nmat.pl
@@ -1,73 +1,79 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
-my $cluster = 10189;
-my $path = '/home/dsellis/data/IES_data/msas/alignments/aln/cluster.';
-my $charMatF = $path.$cluster.'.F.dat';
+
+my $path  = '/home/dsellis/data/IES_data/msas/alignments/aln/';
+
+opendir(DH, $path) or die $!;
+my @files = grep {/\.F\.dat/} readdir(DH);
+foreach my $file(@files){
+    my $outfile = $file;
+    $outfile =~ s/\.F\.dat$/.N.dat/;
+    print $outfile,"\n";
+    my $charMatF = $path.$file;
 #make character matrix based on nucleotide sequence coordinates
 #    read character matrices (aa location and frame). if IES too close (calculate in bp) read IES sequence file and check ends of sequence and MAC sequence
-
-open IN, $charMatF or die $!; #read character matrix
-my $lineCounter = 0;
-my @header;
-my %geneIESH;
-my %allNlocH; # hash to fit all unique locations in nucl. coordinates
-while (my $line = <IN>){
-    chomp $line;
-    my @lineAr = split " ", $line;
-    if($lineCounter == 0){ #header
-	shift @lineAr; #remove genename
-	@header = @lineAr;
-# find if ies are closer than 2aa to each other
-    }else{
-# find if ies have a different insertion frame
-	my $geneName = shift @lineAr;
-	my @frames = @lineAr;
-	my $colCounter = 0;
-	foreach my $frame (@frames){
-	    my $aaloc;
-	    my $Nloc = 0;
-	    if ($frame !=0){
-		$aaloc = $header[$colCounter];
-		$Nloc = $aaloc*3 - (3 - $frame);  #Nloc = aaloc*3 - (3-frame)	    
-		$allNlocH{$Nloc} = 1;
-                #create hash {geneName} = [nloc1, nloc2]
-		if(defined()){
-		    push @{$geneIESH{$geneName}}, $Nloc;
-		}else{
-		    $geneIESH{$geneName} = [$Nloc];
+    
+    open IN, $charMatF or die $!; #read character matrix
+    my $lineCounter = 0;
+    my @header;
+    my %geneIESH;
+    my %allNlocH; # hash to fit all unique locations in nucl. coordinates
+    while (my $line = <IN>){
+	chomp $line;
+	my @lineAr = split " ", $line;
+	if($lineCounter == 0){ #header
+	    shift @lineAr; #remove genename
+	    @header = @lineAr;
+	}else{
+	    my $geneName = shift @lineAr;
+	    my @frames = @lineAr;
+	    my $colCounter = 0;
+	    foreach my $frame (@frames){
+		my $aaloc;
+		my $Nloc = 0;
+		if ($frame != 0){
+		    $aaloc = $header[$colCounter];
+		    $Nloc = $aaloc*3 - (3 - $frame);  #Nloc = aaloc*3 - (3-frame)	    
+		    $allNlocH{$Nloc} = 1;
+		    #create hash {geneName} = [nloc1, nloc2]
+		    if(defined($geneIESH{$geneName})){
+#			print "@{$geneIESH{$geneName}}.\n";
+			push @{$geneIESH{$geneName}}, $Nloc;
+		    }else{
+			$geneIESH{$geneName} = [$Nloc];
+		    }
 		}
+		$colCounter++;
 	    }
-	    $colCounter++;
-	}
 # find distances (in pb) of consecutive IES
 # if they are too close to each other test for floating
-    }
-    $lineCounter++;
-}
-close IN;
-
-# second pass go through again and print
-my @newHeader;
-#push @newHeader, 'geneName';
-foreach my $nl (sort{$a <=> $b} keys %allNlocH){
-    push @newHeader, $nl;
-}
-
-print 'geneName ',"@newHeader\n";
-foreach my $gn (keys %geneIESH){
-    print $gn,' ';
-    foreach my $nloc (@newHeader){
-	foreach my $iesL (@{$geneIESH{$gn}}){
-	    if($iesL == $nloc){
-#		die $iesL;
-		print 1;
-	    }else{
-		print 0;
-	    }
-	    print ' ';
 	}
+	$lineCounter++;
     }
-    print "\n";
+    close IN;
+# second pass go through again and print
+    my @newHeader;
+#push @newHeader, 'geneName';
+    foreach my $nl (sort{$a <=> $b} keys %allNlocH){
+	push @newHeader, $nl;
+    }
+    open OUT, '>'.$path.$outfile or die $!;
+    print OUT 'geneName',"\t",join("\t",@newHeader),"\n";
+    foreach my $gn (keys %geneIESH){
+	print OUT $gn,"\t";
+	foreach my $nloc (@newHeader){
+	    my $flag = 0;
+	    foreach my $iesL (@{$geneIESH{$gn}}){
+		if($iesL == $nloc){
+		    $flag = 1;
+		}
+	    }
+	    print OUT $flag;
+	    print OUT "\t";
+
+	}
+	print OUT "\n";
+    }
+    close OUT;
 }
- 
