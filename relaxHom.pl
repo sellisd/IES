8bf96c57cd19ad57e006189e3702c625abbc24ff
Diamantis Sellis <sellisd@gmail.com>
2015-03-02 11:59:00 +0100

    fixed bug in relaxHom.pl

--- relaxHom.pl
+++ relaxHom.pl
@@ -28,26 +28,23 @@ foreach my $file (@charMF){
 	$rowCounter++;
     }
     close CM;
-
     my @NM;
     my $mergeF = 'cluster.'.$cluster.'.2merge.bed';
     my @NMcolNames;
     if(-s $path.$mergeF){
-	print $path.$mergeF,"\n";
-
-# #print Matrix
-	print "M:\n";
-	print join("\t",@colNames),"\n";
-	for(my $i = 0; $i <= $#M; $i++){ # new matrix has the same number of rows
-	    print $rowNames[$i],"\t";
-	    for(my $j = 0; $j <= $#{$M[0]}; $j++){
-		print $M[$i][$j],"\t";
-	    }
-	    print "\n";
-	}
-
-
+ # #print Matrix
+ 	# print "M:\n";
+ 	# print join("\t",@colNames),"\n";
+ 	# for(my $i = 0; $i <= $#M; $i++){ # new matrix has the same number of rows
+ 	#     print $rowNames[$i],"\t";
+ 	#     for(my $j = 0; $j <= $#{$M[0]}; $j++){
+ 	# 	print $M[$i][$j],"\t";
+ 	#     }
+ 	#     print "\n";
+ 	# }
+#read 2merge file and build NM table
 	open IN, $path.$mergeF or die $!;
+	my $colCor = 0; #keep track of the NM and M column correspondence
 	while(my $line = <IN>){
 	    chomp $line;
 	    (my $cl, my $start, my $end, my $cols) = split " ", $line;
@@ -67,28 +64,38 @@ foreach my $file (@charMF){
 		    }
 		    my $mergedStringS = join (',',@mergedString);
                     #new matrix gets merged column entries in place of the first of the merged ones
-		    $NM[$i][$merged[0]] = $mergedStringS;
+		    $NM[$i][$merged[0]-$colCor] = $mergedStringS;
 		}
+		$colCor+=$#merged;
 	    }else{
 		for(my $i = 0; $i <= $#M; $i++){
-		    $NM[$i][$cols] = $M[$i][$cols];#new matrix has the same column
+		    $NM[$i][$cols-$colCor] = $M[$i][$cols];#new matrix has the same column
 		}
 	    }
 	}
+	 # for(my $ci = 0; $ci<=$#NM; $ci++){
+	 #     print $ci,"\t";
+	 #     for(my $cj = 0; $cj <= $#{$NM[0]}; $cj++){
+	 # 	my $Dentry = $NM[$ci][$cj];
+	 # 	if(defined($Dentry)){
+	 # 	    print $Dentry,"\t";
+	 # 	}else{
+	 # 	    print "NA","\t";
+	 # 	}
+	 #     }
+	 #     print "\n";
+	 # }
 	close IN;
 	open OUT, '>'.$pathOut.'cluster.'.$cluster.'.dat' or die $!;
-	print "Cluster $cluster:\n";
+	print "Cluster $cluster\n";
 	print OUT "geneName\t",join("\t",@NMcolNames),"\n";
-	for(my $i = 0; $i <= $#M; $i++){ # new matrix has the same number of rows
+	for(my $i = 0; $i <= $#NM; $i++){ # new matrix has the same number of rows
 	    print OUT $rowNames[$i],"\t";
 	    for(my $j = 0; $j <= $#{$NM[0]}; $j++){
-		if(!defined($NM[$i][$j])){
-		    print $cluster,"\n";
-		    use Data::Dumper;
-		    print Dumper @NM;
-		    die;
-}
-		print OUT $NM[$i][$j],"\t";
+		if(!defined($NM[$i][$j])){ #columns that our outside of Gblocks are not defined
+		}else{
+		    print OUT $NM[$i][$j],"\t";
+		}
 	    }
 	    print OUT "\n";
 	}
