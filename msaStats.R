fe26e4619a84a65088d609f9dc86741d907f2673
Diamantis Sellis <sellisd@gmail.com>
2015-07-09 11:10:39 +0200

    add msaStats and adjust to current file standards

---
+++ msaStats.R
@@ -0,0 +1,30 @@
+#source("/home/dsellis/projects/fgmo/colors.R")
+library(seqinr)
+library(ape)
+#colorCodes<-c("PCAU"=cbrown1,"PBIA"=cred1,"PTET"=cgreen1,"PSEX"=cblue1,"TTHE"= "black")
+#pathT <- "/home/dsellis/data/IES_data/msas/alignments/dnd/"
+pathA <- "/home/dsellis/data/IES_data/msas/alignments/aln/"
+filesF <- dir(path=pathA,pattern="*.aln")
+m <- data.frame(matrix(nrow=length(filesF),ncol=4))
+#pdf("/home/dsellis/projects/IES/allTrees.pdf")
+#par(mfcol=c(2,2))
+counter <- 1
+for(f in filesF){
+  print(paste(counter,length(filesF)))
+  #alnF <- gsub(".dnd",".aln",f)
+  b <- seqinr::read.alignment(paste(pathA,f,sep=""),format="clustal")
+  cns <- seqinr::consensus(b,method="threshold",threshold=1)
+  seqL <- length(cns) # length of consensus
+  seqN <- b$nb #number of sequences
+  ident <- 100*(1-sum(is.na(cns))/length(cns))
+  m[counter,] <- c(f, seqN, seqL, ident)
+  counter <- counter + 1
+  #a<-read.tree(paste(pathT,f,sep=""))  
+  #if(length(a$tip.label)>10){
+  #   plot.phylo(a,tip.color=colorCodes[substr(a$tip.label,0,4)],cex=0.5)
+  #   title(f)
+  # }
+}
+#dev.off()
+write.table(m,file="/home/dsellis/projects/IES/reports/pipeline/msaStats.dat",row.names=FALSE,col.names=FALSE,quote=FALSE)
+
--- prepareMultibiphy.pl
+++ prepareMultibiphy.pl
@@ -1,10 +1,14 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use File::Path qw(make_path);
+#use Parallel::ForkManager;
+
+#my $pm = Parallel::ForkManager->new(7);
 
 #prepare and run biphy
 
-my $treePath = '/home/dsellis/data/IES_data/msas/phyldog1/results/';
+my $treePath = '/home/dsellis/data/IES_data/msas/phyldog/results/';
 my $matricesPath = '/home/dsellis/data/IES_data/msas/alignments/charMatphy/';
 my $matricesASRP = '/home/dsellis/data/IES_data/msas/asr/matrices/';
 my $treeFile ='/home/dsellis/data/IES_data/msas/asr/allTrees.tre';
@@ -13,14 +17,23 @@ my $biphyPath = '/home/dsellis/tools/biphy-master/';
 # read all output trees from Phyldog run
 # concatenate them to one file if they have a character matrix in phylip format
 
+make_path($matricesASRP) unless -d $matricesASRP;
+
+open TR, '>'.$treeFile or die $!;
+
 opendir DH, $treePath or die "$treePath $!";
 my @files = grep{/^.*.\.ReconciledTree$/} readdir(DH);
 close DH;
-open TR, '>'.$treeFile or die $!;
-my $debug = 0;
+#my $debug = 1;
+
 foreach my $fileName (sort @files){
+#    my $pid = $pm->start and next;
     $fileName =~ /(\d+)\.ReconciledTree$/;
     my $cluster = $1;
+    
+#    $matricesASRP = '/home/dsellis/data/IES_data/msas/asr'.$cluster.'/matrices/';
+#    $treeFile ='/home/dsellis/data/IES_data/msas/asr'.$cluster.'/allTrees.tre';
+ 
     my $matrixF = $matricesPath.'cluster.'.$cluster.'.phy';
     if (-e $matrixF){
 	open IN, $treePath.$fileName or die $!;
@@ -29,17 +42,32 @@ foreach my $fileName (sort @files){
 	print TR @tree;
 	print $cluster,"\n";
         #copy matrix to asr/matrices
+	# open MI, $matrixF or die $!;
+	# open MO,'>'.$matricesASRP or die $!;
+	# while(my $line = <MI>){
+	#     print MO $line;
+	# }
+	# close MI;
+	# close MO;
 	system("cp $matrixF $matricesASRP");
     }else{
 	print $cluster," has no matrix\n";
+#	$pm->finish;
     }
-    if ($debug>10){last;}
-    $debug++;
 
+#    while(! -s "ies.$cluster.trace"){
+#	#wait
+#    }
+#    my $string = `ps -aux|grep ies.$cluster`;
+#    (my $user, my $PID) = (split " ", $string)[0,1];
+#    kill 'KILL', $PID;
+#    $pm->finish;
+#    $debug++;
+#    die if $debug>10;
 }
 close TR;
 my $cmdl;
-#$cmdl = $biphyPath."multibiphy -d $matricesASRP -t $treeFile -a ies";
+# #$cmdl = $biphyPath."multibiphy -d $matricesASRP -t $treeFile -a ies";
 $cmdl = $biphyPath."multibiphy -d $matricesASRP -t $treeFile -a -u 1 ies";
 print $cmdl,"\n";
-#system "$cmdl";
+system "$cmdl";
