7cf2ef38e4c6af279231c54bafcca1e6d8571429
Diamantis Sellis <sellisd@gmail.com>
2015-10-07 15:20:10 +0200

    filterblast.pl: - fix output and bugs in selecting best hits - do not filter low E-value hits - print IES with only self hits and one (random) of the top hits
    lengthBins.R: assign length bins to IESs

--- filterblast.pl
+++ filterblast.pl
@@ -3,21 +3,18 @@ use warnings;
 use strict;
 use Math::Random::Secure qw(irand);
 
-my $file = $ARGV[0];
+my $inputFile =  '/home/dsellis/data/IES_data/iesseq/iesseq.blastout';
+my $outputFile = '/home/dsellis/data/IES_data/iesseq/iesseq.filt.blastout';
 
-my $lengthCutoff = 20;
-my $identityCutoff = 95;
 my $evalueCutoff = 0.001;
 # blast output
 # qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore
 # for each IES keep the lowest e-value and if multiple the lowest bit-score
 my %tophits;
-my %worsehits;
-open IN, $file or die $!;
+my %selfhits;
+open IN, $inputFile or die $!;
 while(my $line = <IN>){
     my @ar = split " ", $line;
-    #next if $ar[2] <= $identityCutoff;
-    #next if $ar[3] <= $lengthCutoff;
     my $currentMatch = {'sseqid' => $ar[1],
 			'pident' => $ar[2],
 			'length' => $ar[3],
@@ -30,30 +27,20 @@ while(my $line = <IN>){
 			'evalue' => $ar[10],
 			'bitscore' => $ar[11],
 			'out of' => 0};
-    if($ar[0] eq $ar[1]){
-#pick the worse self-hits and print separately
-	if(defined($worsehits{$ar[0]})){
-#	    keep the onw with the worse evalue
-	    if($worsehits{$ar[0]}->{'evalue'} < $ar[10]){
-		$worsehits{$ar[0]} = $currentMatch;
-	    }
-	}else{
-	    $worsehits{$ar[0]} = $currentMatch;
-	}
-	next;  # skip self hits
-    }
-    next if $ar[10] >= $evalueCutoff; # keep best hits with high e-value only
+     if($ar[0] eq $ar[1]){
+	 $selfhits{$ar[0]} = 1;
+ 	next;  # skip self hits
+     }
+#    next if $ar[10] >= $evalueCutoff; # keep best hits with high e-value only
     if(defined($tophits{$ar[0]})){ # is this IES previously seen?
-	if($tophits{$ar[0]}->{'evalue'} < $ar[10]){ # if evalue is less
+	if($ar[10] < $tophits{$ar[0]}->{'evalue'}){ # if evalue is less
 	    $tophits{$ar[0]} = $currentMatch;       #     replace
-	}elsif($tophits{$ar[0]}->{'evalue'} == $ar[10]){  # if equal
-	    if($tophits{$ar[0]}->{'bitscore'} > $ar[11]){ # compare bitscore
+	}elsif($ar[10] == $tophits{$ar[0]}->{'evalue'}){  # if equal
+	    if($ar[11] > $tophits{$ar[0]}->{'bitscore'}){ # compare bitscore
 		$tophits{$ar[0]} = $currentMatch;
-	    }elsif($tophits{$ar[0]}->{'bitscore'} == $ar[11]){
-#if same e-value and bitscore
-#pick one randomly
-#		print $ar[0], $ar[1];
-#		die;
+	    }elsif($ar[11] == $tophits{$ar[0]}->{'bitscore'}){
+                #if same e-value and bitscore
+                #pick one randomly
 		my $int = irand(2);
 		if($int == 0){
 		    $currentMatch->{'out of'} = $tophits{$ar[0]}->{'out of'};
@@ -68,44 +55,33 @@ while(my $line = <IN>){
 }
 close IN;
 
-# foreach my $qies (sort keys %worsehits){
-#     print $qies,' ';
-#     print $worsehits{$qies}->{'sseqid'},' ';
-#     print $worsehits{$qies}->{'pident'},' ';
-#     print $worsehits{$qies}->{'length'},' ';
-#     print $worsehits{$qies}->{'mismatch'},' ';
-#     print $worsehits{$qies}->{'evalue'},' ';
-#     print $worsehits{$qies}->{'bitscore'},' ';
-#     print $worsehits{$qies}->{'out of'},' ';
-#     print "\n";
-# }
-
-foreach my $qies (sort keys %tophits){
-    print &iesName2species($qies), ' ';                        # 1 query IES
-    print &iesName2species($tophits{$qies}->{'sseqid'}),' ';   # 2 top hit IES
-    print $tophits{$qies}->{'pident'},' ';                     # 3 pident
-    print $tophits{$qies}->{'length'},' ';                     # 4 length
-    print $tophits{$qies}->{'mismatch'},' ';                   # 5 mismatch
-    print $tophits{$qies}->{'evalue'},' ';                     # 6 e-value
-    print $tophits{$qies}->{'bitscore'},' ';                   # 7 bit score
-    print $tophits{$qies}->{'out of'},' ';                     # 8 No of equally best hits
-    if(defined($worsehits{$qies})){
-	# The self-hit for this IES with the worse e-value
-	print $worsehits{$qies}->{'evalue'},' ';               # 9 e-value of worse self-hit
-    }else{
-	die;
-# There is no self-hit for this IES???
+foreach my $qies (sort keys %selfhits){
+    if(!defined($tophits{$qies})){ # is there an IES with only self-hits?
+	print $qies,"\n";
     }
-    print "\n";
 }
 
+open OUT, '>'.$outputFile or die $!;
+foreach my $qies (sort keys %tophits){
+    print OUT &iesName2species($qies), ' ';                        # 1 query IES
+    print OUT &iesName2species($tophits{$qies}->{'sseqid'}),' ';   # 2 top hit IES
+    print OUT $tophits{$qies}->{'pident'},' ';                     # 3 pident
+    print OUT $tophits{$qies}->{'length'},' ';                     # 4 length
+    print OUT $tophits{$qies}->{'mismatch'},' ';                   # 5 mismatch
+    print OUT $tophits{$qies}->{'evalue'},' ';                     # 6 e-value
+    print OUT $tophits{$qies}->{'bitscore'},' ';                   # 7 bit score
+    print OUT $tophits{$qies}->{'out of'},' ';                     # 8 No of equally best hits
+    print OUT $qies,' ';                                           # 9 IES id
+    print OUT "\n";
+}
+close OUT;
 
 sub iesName2species{
-#1 P. biaurelia
-#2 P. tetraurelia
-#3 P. sexaurelia
-#4 P. caudatum
-#-1 error/unknown
+    #1 P. biaurelia
+    #2 P. tetraurelia
+    #3 P. sexaurelia
+    #4 P. caudatum
+    #-1 error/unknown
     my $iesName = shift @_;
     my $sp = substr($iesName,0,11);
     if($sp eq 'IESMIC.PBIA'){
---
+++ lengthBins.R
@@ -0,0 +1,19 @@
+# classify IES to length classes
+# define length bins based on observations.
+library(IRanges)
+maxLength <- 10000
+peakX <- c(27,38,45,seq(56, maxLength, by = 10))
+startX <- c(18,34,41,seq(52, maxLength, by = 10))
+endX <- startX[-1]
+lengthBins <- IRanges(start = startX[-length(startX)], end = endX-1)
+# read ies lengths
+load("~/data/IES_data/rdb/iesInfo")
+pbiIesLengthsIR <- IRanges(start = pbiD$length, end = pbiD$length)
+pbiBins <- findOverlaps(pbiIesLengthsIR,lengthBins)
+pbiLengthBins <- pbiBins@subjectHits
+names(pbiLengthBins) <- row.names(pbiD)
+
+pteIesLengthsIR <- IRanges(start = pteD$length, end = pteD$length)
+pteBins <- findOverlaps(pteIesLengthsIR,lengthBins)
+pteLengthBins <- pteBins@subjectHits
+names(pteLengthBins) <- row.names(pbiD)
