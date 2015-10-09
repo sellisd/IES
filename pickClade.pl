512bb3664b09c8931a2d76633c3e6f4027ca9474
Diamantis Sellis <sellisd@gmail.com>
2015-10-09 20:14:39 +0200

    add command line options and better output format

--- pickClade.pl
+++ pickClade.pl
@@ -4,16 +4,52 @@ use strict;
 use lib'.';
 use functions;
 use Bio::TreeIO;
-#given a speciation node number print in a reconciled tree the leafs of the node
-# given an output group G and species A1-An of an input group pick subclades of a tree that have a monophyletic clade with the ingroup species A1-An and an outgroup G
-# as PHYLDOG has already calculated the nodes corresponding to speciation events we can pick all the subtrees of this speciation node
+use Getopt::Long;
 
-my $treePath = '/home/dsellis/data/IES_data/msas/phyldog/results/';
-my $matricesPath = '/home/dsellis/data/IES_data/msas/alignments/charMatphy/';
-my @ingroup = qw/Paramecium_biaurelia Paramecium_tetraurelia Paramecium_sexaurelia/;
-my @outgroup = qw/Paramecium_caudatum/;
+my $help;
+my @ingroup;
+my @outgroup;
+my $S;
+my $treePath;
+my $output;
+my $usage  = <<HERE;
+
+Find subtrees rooted at a given speciation node and annotates the tips as as in- or out-groups
+
+usage pickClade.pl [OPTIONS]
+where OPTIONS can be:
+  -ingroup:  comma separated list of ingroup species (no space)
+  -outgroup: comma separated list of outgroup species names (no space)
+  -path:     path where PHYLDOG reconciled trees are located
+  -output:   output file name
+  -s:        
+  -help|?:   this help screen
+
+e.g.
+
+./pickClade.pl -s 0 -ingroup Paramecium_biaurelia,Paramecium_tetraurelia,Paramecium_sexaurelia -outgroup Paramecium_caudatum  -path /home/dsellis/data/IES_data/msas/phyldog/results/ -output /home/dsellis/data/IES_data/rdb/Pca.PbiPtePse.dat
+
+./pickClade.pl -s 4 -ingroup Paramecium_biaurelia,Paramecium_tetraurelia -outgroup Paramecium_sexaurelia  -path /home/dsellis/data/IES_data/msas/phyldog/results/ -output /home/dsellis/data/IES_data/rdb/Pse.PbiPte.dat
+
+HERE
+
+die $usage unless (GetOptions(
+		       'help|?'     => \$help,
+		       's=i'        => \$S,
+		       'ingroup=s'  => \@ingroup,
+		       'outgroup=s' => \@outgroup,
+		       'path=s'     => \$treePath,
+		       'output=s'   => \$output
+		   ));
+
+@ingroup = split(/,/, join(',', @ingroup));
+@outgroup = split(/,/, join(',', @outgroup));
+die $usage if $help;
+die $usage if $#ingroup == -1 or $#outgroup == -1;
+die $usage unless defined($S);
+
 my %group;
-my $S = 0; #number of speciation event
+
 foreach my $species (@ingroup){
     $group{$species} = 1;
 }
@@ -25,9 +61,13 @@ opendir DH, $treePath or die $!;
 my @files = grep {/^.*.\.ReconciledTree$/} readdir(DH);
 close DH;
 
-print "cluster\tgeneId\tgroup\tspecies\n"; #header
+open OUT, '>'.$output or die $!;
+print OUT "cluster\tgeneId\tgroup\tspecies\n"; #header
+
+my $fileCounter = 0;
 foreach my $fileName(sort @files){
     $fileName =~ /(\d+)\.ReconciledTree$/;
+    print $fileCounter, '/', $#files, "\r";
     my $cluster = $1;
     my $input = new Bio::TreeIO(-file   => $treePath.$fileName,
 				-format => "nhx");
@@ -68,15 +108,17 @@ foreach my $fileName(sort @files){
 		}
 	    }
 	    foreach my $geneId (@ingroup){
-		print $cluster, "\t", $geneId, "\t", 'ingroup', "\t", gene2species($geneId),"\n";
+		print OUT $cluster, "\t", $geneId, "\t", 'ingroup', "\t", gene2species($geneId),"\n";
 	    }
 	    foreach my $geneId (@outgroup){
-		print $cluster, "\t", $geneId, "\t", 'outgroup', "\t", gene2species($geneId),"\n";
+		print OUT $cluster, "\t", $geneId, "\t", 'outgroup', "\t", gene2species($geneId),"\n";
 	    }
 	}
     }
+    $fileCounter++;
 }
 
+close OUT;
 
 #tidy output should be:
 # cluster geneId group    species
