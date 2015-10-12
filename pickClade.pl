1ef4d9391194b9a7c8e6734799c350fee8d03c4d
Diamantis Sellis <sellisd@gmail.com>
2015-10-12 09:44:16 +0200

    provide descriptive error in cases where the speciation node and input/output groupings in the command line are not correct

--- pickClade.pl
+++ pickClade.pl
@@ -18,29 +18,29 @@ Find subtrees rooted at a given speciation node and annotates the tips as as in-
 
 usage pickClade.pl [OPTIONS]
 where OPTIONS can be:
-  -ingroup:  comma separated list of ingroup species (no space)
-  -outgroup: comma separated list of outgroup species names (no space)
-  -path:     path where PHYLDOG reconciled trees are located
-  -output:   output file name
-  -s:        
-  -help|?:   this help screen
+-ingroup:  comma separated list of ingroup species (no space)
+-outgroup: comma separated list of outgroup species names (no space)
+-path:     path where PHYLDOG reconciled trees are located
+-output:   output file name
+-s:        
+-help|?:   this help screen
 
 e.g.
 
-./pickClade.pl -s 0 -ingroup Paramecium_biaurelia,Paramecium_tetraurelia,Paramecium_sexaurelia -outgroup Paramecium_caudatum  -path /home/dsellis/data/IES_data/msas/phyldog/results/ -output /home/dsellis/data/IES_data/rdb/Pca.PbiPtePse.dat
+./pickClade.pl -s 0 -ingroup Paramecium_biaurelia,Paramecium_tetraurelia,Paramecium_sexaurelia -outgroup Paramecium_caudatum  -path ~/data/IES_data/msas/phyldog/results/ -output ~/data/IES_data/rdb/Pca.PbiPtePse.dat
 
-./pickClade.pl -s 4 -ingroup Paramecium_biaurelia,Paramecium_tetraurelia -outgroup Paramecium_sexaurelia  -path /home/dsellis/data/IES_data/msas/phyldog/results/ -output /home/dsellis/data/IES_data/rdb/Pse.PbiPte.dat
+./pickClade.pl -s 2 -ingroup Paramecium_biaurelia,Paramecium_tetraurelia -outgroup Paramecium_sexaurelia  -path ~/data/IES_data/msas/phyldog/results/ -output ~/data/IES_data/rdb/Pse.PbiPte.dat
 
 HERE
 
-die $usage unless (GetOptions(
-		       'help|?'     => \$help,
-		       's=i'        => \$S,
-		       'ingroup=s'  => \@ingroup,
-		       'outgroup=s' => \@outgroup,
-		       'path=s'     => \$treePath,
-		       'output=s'   => \$output
-		   ));
+    die $usage unless (GetOptions(
+			   'help|?'     => \$help,
+			   's=i'        => \$S,
+			   'ingroup=s'  => \@ingroup,
+			   'outgroup=s' => \@outgroup,
+			   'path=s'     => \$treePath,
+			   'output=s'   => \$output
+		       ));
 
 @ingroup = split(/,/, join(',', @ingroup));
 @outgroup = split(/,/, join(',', @outgroup));
@@ -66,56 +66,56 @@ print OUT "cluster\tgeneId\tgroup\tspecies\n"; #header
 
 my $fileCounter = 0;
 foreach my $fileName(sort @files){
-    $fileName =~ /(\d+)\.ReconciledTree$/;
-    print $fileCounter, '/', $#files, "\r";
-    my $cluster = $1;
-    my $input = new Bio::TreeIO(-file   => $treePath.$fileName,
-				-format => "nhx");
-    my $tree = $input->next_tree;
-    for my $nodeO ( $tree->get_nodes ) {
-	my $speciationEvent = $nodeO->get_tag_values('S');
-	my $eventType = $nodeO->get_tag_values('Ev');
-	if($eventType eq 'S' and $speciationEvent == $S){
-	    my @outgroup;
-	    my @ingroup;
-	    for my $child ($nodeO->each_Descendent){
-		my @leaves;
-		# should either be ingroup or outgroup
-		if($child->is_Leaf){
-		    push @leaves, scalar($child->id);
-		}else{
-		    for my $subTreeNode ($child->get_all_Descendents){
-			if ($subTreeNode->is_Leaf){
-			    push @leaves, scalar($subTreeNode->id);
-			}
+  $fileName =~ /(\d+)\.ReconciledTree$/;
+  print $fileCounter, '/', $#files, "\r";
+  my $cluster = $1;
+  my $input = new Bio::TreeIO(-file   => $treePath.$fileName,
+    -format => "nhx");
+  my $tree = $input->next_tree;
+  for my $nodeO ( $tree->get_nodes ) {
+    my $speciationEvent = $nodeO->get_tag_values('S');
+    my $eventType = $nodeO->get_tag_values('Ev');
+    if($eventType eq 'S' and $speciationEvent == $S){
+	my @outgroup;
+	my @ingroup;
+	for my $child ($nodeO->each_Descendent){
+	    my @leaves;
+	    # should either be ingroup or outgroup
+	    if($child->is_Leaf){
+		push @leaves, scalar($child->id);
+	    }else{
+		for my $subTreeNode ($child->get_all_Descendents){
+		    if ($subTreeNode->is_Leaf){
+			push @leaves, scalar($subTreeNode->id);
 		    }
 		}
-		my $group =  &assignGroup(\@leaves);
-		if ($group eq 'ingroup'){
-		    if(@ingroup){
-			die;
-		    }else{
-			@ingroup = @leaves;
-		    }
-		}elsif ($group eq 'outgroup'){
-		    if(@outgroup){
-			die;
-		    }else{
-			@outgroup = @leaves;
-		    }
+	    }
+	    my $group =  &assignGroup(\@leaves);
+	    if ($group eq 'ingroup'){ 
+		if(@ingroup){
+		    die "Multiple ingroups from the current speciation node. Perhaps the speciation node and the ingroup/outgroup were not defined properly";
 		}else{
-		    die;
+		    @ingroup = @leaves;
 		}
-	    }
-	    foreach my $geneId (@ingroup){
-		print OUT $cluster, "\t", $geneId, "\t", 'ingroup', "\t", gene2species($geneId),"\n";
-	    }
-	    foreach my $geneId (@outgroup){
-		print OUT $cluster, "\t", $geneId, "\t", 'outgroup', "\t", gene2species($geneId),"\n";
+	    }elsif ($group eq 'outgroup'){
+		if(@outgroup){
+		    die "Multiple outgroups from the current speciation node. Perhaps the speciation node and the ingroup/outgroup were not defined properly";
+		}else{
+		    @outgroup = @leaves;
+		}
+	    }else{
+		die;
 	    }
 	}
+	foreach my $geneId (@ingroup){
+	    print OUT $cluster, "\t", $geneId, "\t", 'ingroup', "\t", gene2species($geneId),"\n";
+	}
+	foreach my $geneId (@outgroup){
+	    print OUT $cluster, "\t", $geneId, "\t", 'outgroup', "\t", gene2species($geneId),"\n";
+	}
     }
-    $fileCounter++;
+  }
+  $fileCounter++;
 }
 
 close OUT;
@@ -124,6 +124,7 @@ close OUT;
 # cluster geneId group    species
 # 100     PGM..  ingroup  PTET
 # 100     MIC..  outgroup PCAU
+
 sub assignGroup{
     my $arref = shift @_;
     my $counter = 0;
@@ -131,7 +132,7 @@ sub assignGroup{
     foreach my $element (@$arref){
 	$group += &isIngroup($element);
 	$counter++;
-#	print $group, ' ', $element,"\n";
+	#  print $group, ' ', $element,"\n";
     }
     if($counter == $group){
 	return 'ingroup';
@@ -148,6 +149,6 @@ sub isIngroup{
     if(defined($group{$species})){
 	return $group{$species};
     }else{
-	die;
+        die;
     }
 }
