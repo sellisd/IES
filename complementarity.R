52b5ee26c25a158198a35ad8abcee6628096dbc5
Diamantis Sellis <sellisd@gmail.com>
2015-12-08 13:57:43 +0100

    calculate complementarity of boundary and flanking region sequences

---
+++ complementarity.R
@@ -0,0 +1,9 @@
+# calculate complementarity of IES boundaries and flanking regions
+load("~/data/IES_data/rdb/iesInfo")
+load("~/data/IES_data/rdb/lengthBins")
+
+pbiL <- boundaryCompl(pbiD, pbiLengthBins)
+pteL <- boundaryCompl(pteD, pteLengthBins)
+pseL <- boundaryCompl(pseD, pseLengthBins)
+pcaL <- boundaryCompl(pcaD, pcaLengthBins)
+save(pbiL, pteL, pseL, pcaL, file = "~/data/IES_data/rdb/complementarity")
