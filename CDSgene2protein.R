5b213fde57cd81320dba983b4e40f073bf09280a
Diamantis Sellis <sellisd@gmail.com>
2016-02-23 17:56:22 +0100

    fix input output

--- CDSgene2protein.R
+++ CDSgene2protein.R
@@ -1,9 +1,11 @@
 # read cds coordinates in gene coordinates and calculate the corresponding protein coordinates
+args<-commandArgs(TRUE);
+cdsFile <- args[1]
+outFile <- args[2]
+#cdsFile "~/data/IES_data/ptetraurelia/pte.cds"
+#outFile "~/data/IES_data/ptetraurelia/pte.cds.prot"
+#Rscript --vanilla CDSgene2protein.R cdsFile outFile
 source("~/projects/IES/src/sharedFunctions.R")
-ptecds <- read.table("~/data/IES_data/ptetraurelia/pte.cds", header = TRUE, stringsAsFactors = FALSE)
-ptecdsprot <- gene2protCDS(ptecds)
-write.table(ptecdsprot, file = "~/data/IES_data/ptetraurelia/pte.cds.prot", row.names = FALSE, quote = FALSE, sep = "\t")
-
-pbicds <- read.table("~/data/IES_data/pbiaurelia/pbi.cds", header = TRUE, stringsAsFactors = FALSE)
-pbicdsprot <- gene2protCDS(pbicds)
-write.table(pbicdsprot, file = "~/data/IES_data/pbiaurelia/pbi.cds.prot", row.names = FALSE, quote = FALSE, sep = "\t")
+cds <- read.table(cdsFile, header = TRUE, stringsAsFactors = FALSE)
+cdsprot <- gene2protCDS(cds)
+write.table(cdsprot, file = outFile, row.names = FALSE, quote = FALSE, sep = "\t")
