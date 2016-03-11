62ee9c34d0d6010d070282a75cffb21f14797ec7
Diamantis Sellis <sellisd@gmail.com>
2016-03-11 11:30:42 +0100

    rewrite for more fine error checking of runs on the cluster. Unify error checking and create file to run locally all failed or interrupted runs

--- blastWarn.pl
+++ blastWarn.pl
@@ -3,49 +3,77 @@ use warnings;
 use strict;
 
 opendir(DH, $ARGV[0]) or die $!;
-my @files = readdir(DH);
+my @files = grep{/.*\.pbs/} readdir(DH);
 close DH;
-
-print `ls -1 output.* |wc`;
-print `ls -1 error.* |wc`;
-print `ls -1 *.pbs|wc`;
-#print `echo telos> 4comp; diff --from-file=4comp output.*; rm 4comp`;
-my %output;
-my %pbs;
+my %errors;
 
 foreach my $file (@files){
-    if ($file =~ /output\.(\d+)/){
-	$output{$1} = $file;
-	open OUT, $file or die $!;
+    #check if pbs file has a correctly formated output and an empty error file
+    $file =~ /(.*).pbs/;
+    my $chunk = $1;
+    my $outputFile = 'output.'.$chunk;
+    my $errorFile = 'error.'.$chunk;
+    if(-e $outputFile){
+	open OUT, $outputFile or die $!;
 	my $lineCounter = 0;
+	my $validOutput = 0;
 	while(my $line = <OUT>){
+	    chomp $line;
 	    if ($lineCounter == 1){
 		chomp $line;
-		print "Warning, strange output at $file\n" unless $line eq 'telos';
+		if ($line eq 'telos'){
+		    $validOutput = 1;
+		}
 	    }
 	    $lineCounter++;
 	}
 	close OUT;
-    }elsif($file =~ /(\d+).pbs/){
-	$pbs{$1} = $file;
+	if ($validOutput == 0){
+	    &addErrorMsg($chunk, 'Not valid output');
+	}
+    }else{
+	&addErrorMsg($chunk, 'Not run');
     }
-    next unless $file =~ /error\.(\d+)/;
-    open ER, $file or die $!;
-    my $lineCounter = 0;
-    while (my $line = <ER>){
-	chomp $line;
-	if ($line =~/^Selenocysteine \(U\) at position \d+ replaced by X$/){
-	}elsif($line =~ /Warning: One or more U or O characters replaced by X for alignment score calculations at positions/){
-	}elsif($line eq ''){
-	}else{
-	    print "Error at $file line $lineCounter\n";
-        }
-        $lineCounter++;
+    if(-s $errorFile){
+	open ER, $errorFile or die $!;
+	my $lineCounter = 0;
+	while (my $line = <ER>){
+	    chomp $line;
+	    if ($line =~/^Selenocysteine \(U\) at position \d+ replaced by X$/){
+		# ignore warning
+	    }elsif($line =~ /Warning: One or more U or O characters replaced by X for alignment score calculations at positions/){
+		# ignore warning
+	    }elsif($line eq ''){
+		# ignore empty lines
+	    }else{
+		&addErrorMsg($chunk, 'Interrupted');
+	    }
+	    $lineCounter++;
+	}
+	close ER;
     }
-    close ER;
 }
-foreach my $run (keys %pbs){
-    unless(defined($output{$run})){
-	print "Warning: ", $run, " did not finish\n";
+
+# print summary of  errors
+
+foreach my $chunks (keys %errors){
+    print "$chunks, @{$errors{$chunks}}\n"
+}
+
+# prepare to rerun locally all the failed runs or rerun in another queue?
+
+open RL, '>runlocal.sh' or die $!;
+foreach my $run (keys %errors){
+    print RL 'blastp -query /home/dsellis/data/IES/tempdat/fastachunks/chunk.'.$run.'.fa -db /home/dsellis/data/IES/analysis/protdb/allprot -outfmt 6 -out /home/dsellis/data/IES/analysis/allvsall/blastout.chunk.'.$run.'.fa.dat -seg yes > '.$run.'.log &',"\n";
+}
+close RL;
+
+sub addErrorMsg{
+    my $code = shift @_;
+    my $message = shift @_;
+    if(defined($errors{$code})){
+	push @{$errors{$code}}, $message;
+    }else{
+	$errors{$code} = [$message];
     }
 }
