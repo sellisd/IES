8044f87a581362faad8d967f2ce7de6314bf47aa
Diamantis Sellis <sellisd@gmail.com>
2016-04-15 16:59:28 +0200

    adjust input output to new pipeline

--- filterProtAlign.R
+++ filterProtAlign.R
@@ -1,20 +1,20 @@
 library(seqinr)
-path <- "/home/dsellis/data/IES_data/msas/alignments/aln/"
-filteredPath <- "/home/dsellis/data/IES_data/msas/alignments/filtered/"
+path <- "/home/dsellis/data/IES/analysis/msas/aln/"
+filteredPath <- "/home/dsellis/data/IES/analysis/msas/filtered/"
 if(!dir.exists(filteredPath)){
   dir.create(filteredPath)
 }
-files <- dir(path= path,pattern="*.aln$")
+files <- dir(path= path,pattern="*.aln.fa$")
 nb <- numeric()
 id <- numeric()
 for(fileName in files){
   print(fileName)
-  a <- read.alignment(paste(path,fileName,sep=""),format="clustal")
+  a <- read.alignment(paste(path,fileName,sep=""),format="fasta")
   nb <- append(nb,a$nb) #number of sequeces
   distM <- dist.alignment(a,matrix="identity")
   id <- append(id,mean(1-distM*distM)) #average pairwise identity
 }
-write.table(data.frame(file=files,sequenceNo=nb,avPairId=id),file="~/data/IES_data/msas/protAlignStats.dat", quote=FALSE)
+write.table(data.frame(file=files,sequenceNo=nb,avPairId=id),file="~/data/IES/analysis/msas/protAlignStats.dat", quote=FALSE)
 highId <- files[which(id>0.5)]
 enoughSeq <- files[which(nb>3)]
 filtered <- intersect(enoughSeq,highId)
