c2e66f069b6b5c8f5300a336d07937d5cbcf8c26
Diamantis Sellis <sellisd@gmail.com>
2016-04-19 13:13:11 +0200

    replace hardcoded file and species parameters calls to getNotation in functions

--- step1.pl
+++ step1.pl
@@ -12,27 +12,8 @@ use strict;
 my $homeD = File::HomeDir->my_home;
 my $notationF =  catfile($homeD, 'data/IES/analysis/notation.tab');
 
-open N, $notationF or die $!;
-my $header = readline(N);
+my $nr = getNotation($notationF);
 my %notation;
-while(my $line = <N>){
-    chomp $line;
-(my $abbreviation, my $datapath, my $binomial, my $taxId, my $geneGff, my $cdsF, my $protF, my $geneF, my $MacF, my $iesGff, my $annotation, my $prefix) = split "\t", $line;
-    $notation{$binomial} = {
-	'annotation' => $annotation,
-	'prefix'     => $prefix,
-	'abr'        => $abbreviation,
-	'datapath'   => $datapath,
-	'taxId'      => $taxId,
-	'geneGff'    => $geneGff,
-	'cdsF'       => $cdsF,
-	'protF'      => $protF,
-	'geneF'      => $geneF,
-	'MacF'       => $MacF,
-        'iesGff'     => $iesGff
-    };
-}
-close N;
 
 # Variables
 my $scafCutoff = 10_000;
@@ -40,62 +21,62 @@ my $scafCutoff = 10_000;
 
 # calculate scaffold lengths
 # --------------------------
-if(0){
-system("./scaffoldStats.pl /home/dsellis/data/IES/primaurelia/genome/pprimaurelia_mac_AZ9-3_v1.0.fa        > /home/dsellis/data/IES/analysis/ppr.scaf");
-system("./scaffoldStats.pl /home/dsellis/data/IES/biaurelia/genome/biaurelia_V1-4_assembly_v1.fasta        > /home/dsellis/data/IES/analysis/pbi.scaf");
-system("./scaffoldStats.pl /home/dsellis/data/IES/tetraurelia/genome/ptetraurelia_mac_51.fa                > /home/dsellis/data/IES/analysis/pte.scaf");
-system("./scaffoldStats.pl /home/dsellis/data/IES/pentaurelia/genome/ppentaurelia_mac_87_v1.0.fa           > /home/dsellis/data/IES/analysis/ppe.scaf");
-system("./scaffoldStats.pl /home/dsellis/data/IES/sexaurelia/genome/sexaurelia_AZ8-4_assembly_v1.fasta     > /home/dsellis/data/IES/analysis/pse.scaf");
-system("./scaffoldStats.pl /home/dsellis/data/IES/octaurelia/genome/poctaurelia_mac_138_v1.0.fa            > /home/dsellis/data/IES/analysis/poc.scaf");
-system("./scaffoldStats.pl /home/dsellis/data/IES/tredecaurelia/genome/ptredecaurelia_209_AP38_filtered.fa > /home/dsellis/data/IES/analysis/ptr.scaf");
-system("./scaffoldStats.pl /home/dsellis/data/IES/caudatum/caudatum_43c3d_assembly_v1.fasta >/home/dsellis/data/IES/analysis/pca.scaf");
+foreach my $sp (keys %$nr){
+    my %pab = %{$nr->{$sp}}; #de-reference for less typing
+    my $genome = catfile($pab{'datapath'}, $pab{'MacF'});
+    my $scafF = catfile($pab{'datapath'}, 'analysis', $pab{'abr'}.'.scaf');
+    my $cmdl = './scaffoldStats.pl '.$genome.' > '.$scafF;
+    run($cmdl, 1);
 }
 
 # filter scaffolds
 # ----------------
 
-if(0){
-    foreach my $sp (keys %notation){
-	my $fsref = buildPaths($sp, $notation{$sp}{'annotation'});
-	my @args = (
-	    '-length',  catfile($homeD, $fsref->{'scaffoldF'}),
-	    '-protein', catfile($homeD, $fsref->{'proteinF'}),
-	    '-gff',     catfile($homeD, $fsref->{'anotF'}),
-	    '-species', $fsref->{'pab'},
-	    '-gene',    catfile($homeD, $fsref->{'geneF'}),
-	    '-ies',     catfile($homeD, $fsref->{'iesF'}),
-	    '-outdir',  catfile($homeD, '/data/IES/analysis/filtscaf/'),
-	    '-cutoff',  $scafCutoff
-	    );
-	print "@args\n";
-	system("./filterScaffolds.pl", @args);
-    }
+foreach my $sp (keys %$nr){
+    my %pab = %{$nr->{$sp}};
+    my $scafF   = catfile($pab{'datapath'}, 'analysis', $pab{'abr'}.'.scaf');
+    my $protein = catfile($pab{'datapath'}, $pab{'protF'}),
+    my $gff     = catfile($pab{'datapath'}, $pab{'geneGff'}),
+    my $gene    = catfile($pab{'datapath'}, $pab{'geneF'}),
+    my $ies     = catfile($pab{'datapath'}, $pab{'iesGff'}),
+    my $outdir  = catfile($pab{'datapath'}, 'analysis/filtscaf/'),
+    my $cutoff  = $scafCutoff;
+    my $cmdl = './filterScaffolds.pl'.
+	' -length '.$scafF.
+	' -ies '.$ies.
+	' -protein '.$protein.
+	' -gene '.$gene.
+	' -gff '.$gff.
+	' -cutoff '.$cutoff.
+	' -species '.$sp.
+	' -outdir '.$outdir;
+    run($cmdl, 1);
 }
 
 # make BLAST protein database
 # ---------------------------
 
 #find /home/dsellis/data/IES/analysis/ -name "*.protein.fa"
-if(0){
-    make_path("/home/dsellis/data/IES/analysis/protdb/");
-system('cat /home/dsellis/data/IES/analysis/filtscaf/ptr.protein.fa \
-     /home/dsellis/data/IES/analysis/filtscaf/ppe.protein.fa \
-     /home/dsellis/data/IES/analysis/filtscaf/ppr.protein.fa \
-     /home/dsellis/data/IES/analysis/filtscaf/pca.protein.fa \
-     /home/dsellis/data/IES/analysis/filtscaf/poc.protein.fa \
-     /home/dsellis/data/IES/analysis/filtscaf/pse.protein.fa \
-     /home/dsellis/data/IES/analysis/filtscaf/pte.protein.fa \
-     /home/dsellis/data/IES/analysis/filtscaf/pbi.protein.fa \
-     /home/dsellis/data/IES/thermophila/gene/T_thermophila_June2014.protein.fa \
-     >  /home/dsellis/data/IES/analysis/protdb/allprot.fa');
-system('makeblastdb -in /home/dsellis/data/IES/analysis/protdb/allprot.fa -dbtype prot -out  /home/dsellis/data/IES/analysis/protdb/allprot');
+
+my $protdbP = '/home/dsellis/data/IES/analysis/protdb/';
+my $allprotF = catfile($protdbP, 'allprot.fa');
+my $tthGenome = '/home/dsellis/data/IES/thermophila/gene/T_thermophila_June2014.protein.fa';
+make_path($protdbP) unless -d $protdbP;
+my @proteinF;
+for my $sp (keys %$nr){
+    my %pab = %{$nr->{$sp}};
+    push @proteinF, catfile($pab{'datapath'}, 'analysis','filtscaf',$pab{'abr'}.'.protein.fa');
 }
+push @proteinF, $tthGenome; #add T. thermophila
+my $cmdl = 'cat '.join(' ', @proteinF).' > '.$allprotF;
+run($cmdl, 1);
+$cmdl = 'makeblastdb -in '.$allprotF.' -dbtype prot -out '.catfile($protdbP,'allprot');
+run($cmdl, 1);
 
 # split fasta files for faster processing in parallel
 # ---------------------------------------------------
-if(0){
-    system('./preblast.pl /home/dsellis/data/IES/analysis/protdb/allprot.fa /home/dsellis/data/IES/tempdat/fastachunks/');
-}
+run('./preblast.pl /home/dsellis/data/IES/analysis/protdb/allprot.fa /home/dsellis/data/IES/tempdat/fastachunks/', 1);
+
 
 # parse basic ies information
 #----------------------------
