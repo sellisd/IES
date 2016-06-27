a73571a8726244069063324d4b5fe21316c99733
Diamantis Sellis <sellisd@gmail.com>
2016-06-27 12:02:03 +0200

    Calculate absolute difference of IES length in all pairs of IESs in each group of homologous IESs

---
+++ lengthEvol.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from __future__ import print_function
+import itertools
+
+"""Calculates the absolute difference of IES length (nt) in all pairs of IESs within a group of homologous IES"""
+
+alF = '/home/dsellis/data/IES/analysis/tables/ageLength.dat'
+elF = '/home/dsellis/data/IES/analysis/tables/lengthEvol.dat'
+f = open(alF, 'r')
+f.readline() # header
+d = {}
+for line in f:
+    line = line.rstrip()
+    (abr, iesId, length, homIESId, geneFamily, age) = line.split("\t")
+    d.setdefault((homIESId), []).append(length)
+
+f.close()
+
+o = open(elF, 'w')
+for i in d:
+    for pair in itertools.combinations(d[i], 2):
+        o.write(str(abs(int(pair[0]) - int(pair[1]))) + '\n')
+o.close()
