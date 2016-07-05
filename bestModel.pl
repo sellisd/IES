df85fd6398f380552e652967503e741dd60f3327
Diamantis Sellis <sellisd@gmail.com>
2016-07-05 23:06:58 +0200

    add option to overwrite best models by a user suplied one

--- bestModel.pl
+++ bestModel.pl
@@ -9,6 +9,7 @@ use functions;
 my $help;
 my $nex;
 my $table;
+my $model;
 
 my $usage = <<HERE;
 
@@ -17,13 +18,15 @@ usage bestModel.pl [OPTIONS] INPUTFILE(S)
 where OPTIONS can be:
   -nex:    nexus output file
   -table:  table with gene family information
+  -model:  overwrite best model and use given model for all partitions
   -help|?: this help screen
 
 HERE
 
 die $usage unless (GetOptions('help|?'  => \$help,
 			      'nex=s'   => \$nex,
-			      'table=s' => \$table
+			      'table=s' => \$table,
+			      'model=s' => \$model
 		   ));
 die $usage if $#ARGV < 0;
 die $usage if $help;
@@ -54,6 +57,7 @@ foreach my $inputFile (@ARGV){
 	    $bestModel = $1;
 	}
     }
+    $bestModel = $model if $model;
     $inputFile =~ /cluster\.(\d+)\.nucl\.fa\.renamed$/ or die $inputFile;
     my $geneFamily = $1;
     print TB $inputFile, "\t", $geneFamily, "\t", $hasTth, "\t", $bestModel, "\n";
