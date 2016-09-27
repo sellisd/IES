196b995165b79f9192f4664934db82044c6f12ec
Diamantis Sellis <sellisd@gmail.com>
2016-09-27 14:57:14 +0200

    Calculate the number of events per branch leading to a speciation node

---
+++ phyldogEventSum.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from __future__ import print_function
+from collections import Counter
+import sys
+import re
+"""Calculate number of events per species tree branch."""
+dupl = Counter()
+loss = Counter()
+nodes = set()
+for fn in sys.argv[1:]:
+    f = open(fn, 'r')
+    for line in f:
+        line = line.rstrip()
+        m = re.search('event\((\d+).*/(\d+)\.opt",(duplication|loss)\)', line)
+        if(m):
+            (node, geneFamily, event) = m.group(1,2,3)
+            node = int(node)
+            if(event == 'duplication'):
+                dupl[node] += 1
+                nodes.add(node)
+            else:
+                loss[node] += 1
+                nodes.add(node)
+        else:
+            print(line)
+
+print("speciesNode\tduplications\tlosses")
+for d in sorted(nodes):
+    print(d, dupl[d], loss[d], sep = "\t")
