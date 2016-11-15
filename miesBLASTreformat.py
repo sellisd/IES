1ae85a814ffa9fd0855577010490c114fdd77618
Diamantis Sellis <sellisd@gmail.com>
2016-11-15 10:22:03 +0100

    mobile IES classification

---
+++ miesBLASTreformat.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from __future__ import print_function
+from collections import Counter
+
+inputfile = '/home/dsellis/data/IES/analysis/mies/blastout/mies.blastout'
+outputfile = '/home/dsellis/data/IES/analysis/mies/blastout/mies.blastout.table'
+
+fin = open(inputfile, 'r')
+fout = open(outputfile, 'w')
+hist = Counter()
+clusterSet = set()
+
+for line in fin:
+	line = line.rstrip()
+	(qseqid, sseqid, pident, length, mismatch, gapopen, qstart, qend, sstart, send, evalue, bitscore) = line.split()
+	cluster = qseqid[4:]
+	species = sseqid[0:3]
+	hist[(cluster, species)] += 1
+	clusterSet.add(cluster)
+
+
+spL = ['ppr', 'pbi', 'pte', 'ppe', 'pse', 'poc', 'ptr', 'pso', 'pca']
+fout.write("\t".join(["cluster"] + spL + ["total"]) + "\n")
+for cl in clusterSet:
+    L = []
+    for sp in spL:
+        L.append(hist[(cl, sp)])
+    L = [cl] + L + [sum(L)]
+    fout.write("\t".join(str(x) for x in L) + "\n")
