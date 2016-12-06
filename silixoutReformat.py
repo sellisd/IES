1f7a21c8973f126d5ce8ec1618d36399f0b7c8ac
Diamantis Sellis <sellisd@gmail.com>
2016-12-06 23:23:43 +0100

    add option for filter based on number of species

--- silixoutReformat.py
+++ silixoutReformat.py
@@ -7,9 +7,11 @@ import sys, getopt
 
 inputfile = ''
 outputfile = ''
-usage = "./silixoutReformat.py -i <inputfile> -o <outputfile> -c cutoff"
+cutoff = 0
+spcutof = 0
+usage = "./silixoutReformat.py -i <inputfile> -o <outputfile> -c seqcutoff -s speciesNoCutoff"
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"hi:o:c:",["ifile=","ofile=","cutoff="])
+    opts, args = getopt.getopt(sys.argv[1:],"hi:o:c:s:",["ifile=","ofile=","cutoff=", "sp="])
 except getopt.GetoptError:
     print(usage)
     sys.exit(2)
@@ -23,6 +25,8 @@ for opt, arg in opts:
         outputfile = arg
     elif opt in ("-c", "--cutoff"):
         cutoff = int(arg)
+    elif opt in ("-s", "--sp"):
+        spcutof = int(arg)
 
 hist = Counter()
 clusterSet = set()
@@ -48,9 +52,12 @@ spL = ['ppr', 'pbi', 'pte', 'ppe', 'pse', 'poc', 'ptr', 'pso', 'pca']
 fout.write("\t".join(["cluster"] + spL + ["total"]) + "\n")
 for cl in clusterSet:
     L = []
+    spNo = 0
     for sp in spL:
         L.append(hist[(cl, sp)])
-    if(sum(L) > cutoff):
+        if(hist[(cl,sp)] != 0):
+            spNo += 1
+    if(sum(L) > cutoff and spNo>spcutof):
         L = [cl] + L + [sum(L)]
         fout.write("\t".join(str(x) for x in L) + "\n")
 
