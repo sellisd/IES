526f78a5a3e592cd5a8c22f278a4afd0b20f640a
Diamantis Sellis <sellisd@gmail.com>
2016-12-29 06:47:52 +0100

    relative paths to step0

--- step0.pl
+++ step0.pl
@@ -14,9 +14,9 @@ my $notationF =  catfile($homeD, 'data/IES/analysis/notation.csv');
 my $bedtools = '/usr/local/bin/bedtools';
 
 # output directories
-my $coverageD = '/home/dsellis/data/IES/analysis/coverage';
-my $bedD      = '/home/dsellis/data/IES/analysis/bed';
-my $figuresD  = '/home/dsellis/data/IES/analysis/figures/';
+my $coverageD = catfile($homeD, 'data/IES/analysis/coverage');
+my $bedD      = catfile($homeD, 'data/IES/analysis/bed');
+my $figuresD  = catfile($homeD, 'data/IES/analysis/figures/');
 
 make_path($coverageD) unless -d $coverageD;
 my $nr = getNotation($notationF);
@@ -32,7 +32,7 @@ foreach my $sp (sort keys %$nr){
       ' -ibam '.catfile($pab{'datapath'}, $pab{'micbam'}).
       ' -g '.catfile($pab{'datapath'}, $pab{'MacF'}).
       ' -d > '.$coverageF;
-  run($cmdl, 1);
+  run($cmdl, 0);
   # calculate average per nucleotide read coverage over genes
   run("./geneCov.pl -cov $coverageF -bed $geneBedF -out $geneCovF -avcov $avcovF", 0);
 }
@@ -40,30 +40,4 @@ foreach my $sp (sort keys %$nr){
 
 
 make_path($figuresD) unless -d $figuresD;
-run("Rscript --vanilla ./geneFilterByCoverage.R", 1);
-
-
-
-
-
-# directories
-# my $bamD = '/home/dsellis/data/IES/bam/';
-# my $bedD = '/home/dsellis/data/IES/analysis/bed';
-
-#files
-# my $pcaCovFiltered = catfile($bedD, 'pca.cov.be'); # coverage
-# my $genesF = catfile($bedD, 'pca.gene.be'); # genes
-
-# calculate average per nulcotide coverage over genes
-# my $cmdl = './geneCov.pl > /home/dsellis/data/IES/analysis/tables/pca.gene.cov';
-# run($cmdl, 1);
-# my $pcaCovFiltmerge = catfile($bedD, 'pca.covm.be'); # merged
-# # calculate regions of P. caudatum with genomic coverage > 14x
-# my $cmdl = 'bedtools genomecov -ibam '.
-#     catfile($bamD, 'PCAUD_MIC10_BCP_AFIOSF_2.BOWTIE.pc_mac_43c3d_v1.1.pe.sorted.bam').
-#     ' -bg | awk \'$4 > 14\' > '.
-#     $pcaCovFiltered;
-# run($cmdl, 0);
-
-# $cmdl = "bedtools merge -i $pcaCovFiltered > $pcaCovFiltmerge";
-# run($cmdl, 0);
+run("Rscript --vanilla ./geneFilterByCoverage.R", 0);
