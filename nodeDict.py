eaa16fe374793a1c24ec3e49870d2551881813b1
Diamantis Sellis <sellisd@gmail.com>
2017-02-20 23:13:20 +0100

    Node index for all three species trees.

--- nodeDict.py
+++ nodeDict.py
@@ -4,21 +4,30 @@ from pyies.functions import makeNodeTable
 from pyies.userOptions import basePath
 import os.path
 
-# read list of gene families
-rbNodeIndexes = os.path.join(basePath, 'analysis', 'asr1', 'rbNodeIndexes')
-phyldogResults = os.path.join(basePath, 'analysis', 'phyldogT1', 'results')
+"""Create dictionary of nodeIds."""
 
+print("Reading gene families.")
+geneFamilies = []
 with open(os.path.join(basePath, 'analysis', 'iesdb', 'geneFamilydb.dat'), 'r') as f:
-#f = open('workfile', 'r')
-    #with open('dd', 'r') as f:
     for line in f:
         line = line.rstrip()
         (gfId, seqNo) = line.split("\t")[0:2]
         if seqNo == 'NA':
             continue
-        rbTree = os.path.join(rbNodeIndexes, 'nodeIndex.' + str(gfId) + '.tre')
-        phTree = os.path.join(phyldogResults, str(gfId) + '.ReconciledTree')
-        if(os.path.isfile(rbTree) & os.path.isfile(phTree)):
-             L = makeNodeTable(phyldogTreeF = phTree, revBayesTreeF = rbTree)
-             for (rb, ph) in L:
-                 print("\t".join([str(gfId), rb, ph]))
+        else:
+            geneFamilies.append(gfId)
+
+for asrRun in (1,2,3):
+    print("Processing run " + str(asrRun))
+    rbNodeIndexes = os.path.join(basePath, 'analysis', 'asr' + str(asrRun), 'rbNodeIndexes')
+    phyldogResults = os.path.join(basePath, 'analysis', 'phyldogT' + str(asrRun), 'results')
+    out = os.path.join(basePath, 'analysis', 'tables', 'nodeDictionary' + str(asrRun) + '.dat')
+    with open(out, 'w') as fout:
+        fout.write("\t".join(['cluster', 'r', 'phyldog', 'rb\n']))
+        for gfId in geneFamilies:
+            rbTree = os.path.join(rbNodeIndexes, 'nodeIndex.' + str(gfId) + '.tre')
+            phTree = os.path.join(phyldogResults, str(gfId) + '.ReconciledTree')
+            if(os.path.isfile(rbTree) & os.path.isfile(phTree)):
+                L = makeNodeTable(phyldogTreeF = phTree, revBayesTreeF = rbTree)
+                for (rb, ph) in L:
+                    fout.write("\t".join([str(gfId), 'NA', rb, ph + '\n']))
