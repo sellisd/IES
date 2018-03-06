966fdf929deab77a983deacf6c7cb3cfe5979aac
Diamantis Sellis <sellisd@gmail.com>
2018-03-06 13:54:02 +0100

    Clean up code

--- recentGainLossIES.py
+++ recentGainLossIES.py
@@ -82,6 +82,7 @@ nodeProb = pd.read_csv(nodeProbsFile, sep = "\t", dtype = {'cluster':'str',
                                                             'presence': np.float64})
 
 print("\t".join(['geneFamily', 'iesColumn', 'node']))
+
 if geneFamilyId == None:
     pass
 else:
@@ -92,7 +93,6 @@ else:
         ancestor = prevSpec(leaf)
         ancestorRB = nodeDictionary.phyldog2rb(geneFamilyId, ancestor.ND)
         offspringRB = nodeDictionary.phyldog2rb(geneFamilyId, leaf.ND)
-        #print(ancestorRB + "-" + offspringRB)
         # the probability of presence on the ancestor node
         aS = nodeProb.loc[(nodeProb.cluster == geneFamilyId) & (nodeProb.node == ancestorRB)]
         # the probability of presence on the offspring node
@@ -112,81 +112,3 @@ else:
 # from geneFamilyId, iesColumn and node, find IES
 # node can give us gene
 #The end result would be a table with columns: genefamily, IESColumn, node, where node is the gene name.
-
-quit()
-# output files
-rgl = open(os.path.join(outputPath, 'recentGainLossIES.dat'), 'w')
-
-# read node dictionary
-rb2phyldog = {}
-for line in open(os.path.join(basePath, 'IES/rdb/nodeDictionary.dat')):
-    line = line.rstrip()
-    (cluster, r, phyldog, rb) = line.split()
-    rb2phyldog[(cluster, rb)] = phyldog
-
-# read nodeAsr and make dictionary with cluster node and presence
-f = open('/home/dsellis/data/IES_data/msas/nodeAsr.dat')
-d={} # dictionary with (cluster, node) = { iesCol1: presence, iesCol2:presence...}
-header = f.readline()
-for line in f:
-    line = line.rstrip()
-    (cluster, node, iesColumn, presence) = line.split()
-    if(cluster, node) in rb2phyldog:
-        nodeP = rb2phyldog[(cluster, node)]
-    else:
-#        print("skipping :" + cluster + ', ' + node, file = sys.stderr)
-        continue
-    if (cluster, nodeP) in d:
-        d[(cluster, nodeP)][iesColumn] = presence
-    else:
-        d[(cluster, nodeP)] = {iesColumn : presence}
-
-# for each gene family find terminal branch and walk backwards until the most recent speciation event.
-phyldogPath = '/home/dsellis/data/IES_data/msas/phyldog/results/'
-inputF = open('/home/dsellis/data/IES_data/msas/asr/geneFamilies.dat', 'r')
-
-clusters = inputF.readlines()
-clusters = [i.rstrip() for i in clusters]
-headerOut = '\t'.join(['cluster', 'iesColumn', 'gene', 'fromS', 'toS', 'fromNodeP', 'toNodeP', 'presenceFrom', 'presenceTo', 'eventType']) + '\n'
-fnt.write(headerOut)
-fgl.write(headerOut)
-for cluster in clusters:
-    fileNameString = phyldogPath+str(cluster)+'.ReconciledTree'
-    print(fileNameString)
-    t = Tree(fileNameString)
-#    print(t.get_ascii(attributes=["Ev","S","ND","name"]))
-    for leaf in t:
-        anc = prevSpec(leaf)
-        if(anc):
-            if(cluster, leaf.ND) in d:
-                mykeys = [(cluster, anc.ND), (cluster, leaf.ND)]
-                probs = [d[x] for x in mykeys]
-                # for each ies column
-                iesColumns = list(probs[0].keys())
-                for i in iesColumns:
-                    wasPresent = float(d[(cluster, anc.ND)][i])
-                    isPresent = float(d[(cluster, leaf.ND)][i])
-                    if isPresent == 0 and wasPresent > 0.99:
-                        fgl.write("\t".join([cluster, i, leaf.name, anc.S, leaf.S, anc.ND, leaf.ND, str(wasPresent), str(isPresent), 'loss']) + '\n')
-                    elif wasPresent < 0.01 and isPresent == 1:
-                        fgl.write("\t".join([cluster, i, leaf.name, anc.S, leaf.S, anc.ND, leaf.ND, str(wasPresent), str(isPresent), 'gain']) + '\n')
-                    elif wasPresent < 0.01 and isPresent <0.01:
-                        fnt.write("\t".join([cluster, i, leaf.name, anc.S, leaf.S, anc.ND, leaf.ND, str(wasPresent), str(isPresent), 'absent']) + '\n')
-                    elif wasPresent > 0.99 and isPresent <0.99:
-                        fnt.write("\t".join([cluster, i, leaf.name, anc.S, leaf.S, anc.ND, leaf.ND, str(wasPresent), str(isPresent), 'present']) + '\n')
-                    else:
-                        fnt.write("\t".join([cluster, i, leaf.name, anc.S, leaf.S, anc.ND, leaf.ND, str(wasPresent), str(isPresent), 'notSign']) + '\n')
-        else:
-            pass
-fgl.close()
-fnt.close()
-# try:
-        #     print(leaf.ND + "\t" + anc.ND)
-        # except:
-        #     print(cluster + leaf.name)
-        #     print(leaf)
-        #     print(t.get_ascii(attributes=["Ev","S","ND","name"]))
-        #     quit()
-
-
-# Compare the probability of presence in the two nodes.
