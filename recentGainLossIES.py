7d4dc0cacd35d2b1044f8267ae52a2999827d397
Diamantis Sellis <sellisd@gmail.com>
2018-04-05 13:12:16 +0200

    Introduce class of IES for more detailed classification of event types

--- recentGainLossIES.py
+++ recentGainLossIES.py
@@ -7,6 +7,7 @@ from ete3 import Tree
 import sys, getopt
 import os.path
 import numpy as np
+
 # For each gene tree find terminal(leaf) branches
 # foreach X.ReconciledTree find terminal branches
 
@@ -32,9 +33,8 @@ def prevSpec(nodeO):
 
 
 geneFamilyId = None
-analysis = '2'
-cutoff = 0.95
-outputFile = "./recentLoss" + analysis + ".dat"
+analysis = '3'
+outputFile = "./recentEvents" + analysis + ".dat"
 usage = """
 usage:
 
@@ -42,12 +42,8 @@ usage:
 
     where OPTIONS can be any of the following:
     -g string       Gene family Id, if None provided use all gene families
-    -a [1|2|3]      Choice of species tree analysis to use (Default: 2)
-    -o outputFile   Path and file name for output (Default: './recentLoss'+ a + '.dat')
-    -c float        Absolute cutoff of probability change:
-                    if P_offspring - P_ancestral < - cutoff: loss
-                    if P_offspring - P_ancestral > cutoff  : gain
-                    else                                   : undetermined
+    -a [1|2|3]      Choice of species tree analysis to use (Default: 3)
+    -o outputFile   Path and file name for output (Default: './recentEvents'+ a + '.dat')
     -h              This help screen
 """
 
@@ -62,8 +58,6 @@ for opt, arg in opts:
         sys.exit(1)
     elif opt == '-g':
         geneFamilyId = arg
-    elif opt == '-c':
-        cutoff = float(arg)
     elif opt == '-a':
         analysis = arg
     elif opt == "-o":
@@ -86,15 +80,11 @@ nodeProb = pd.read_csv(nodeProbsFile, sep = "\t", dtype = {'cluster':'str',
 with open(outputFile, 'w') as f:
     f.write("\t".join(['geneFamily', 'iesColumn', 'node\n']))
 
-def printRecentIESLoss(geneFamilyId = 10000, analysis = 2, cutoff = 0.95):
+def printRecentIESLoss(geneFamilyId = 10000, analysis = 2):
     """Extract the recent IES loss
     Args:
     geneFamilyId:  string  Unique id of gene family
     analysis:      string  Choice of species tree analysis to use (Default: 2)
-    -c float        Absolute cutoff of probability change:
-                    if P_offspring - P_ancestral < - cutoff: loss
-                    if P_offspring - P_ancestral > cutoff  : gain
-                    else                                   : undetermined
     """
     phyldogTreeFile = os.path.join(basePath, "analysis", "phyldogT" + analysis, "results", geneFamilyId + ".ReconciledTree")
     t = Tree(phyldogTreeFile)
@@ -115,29 +105,36 @@ def printRecentIESLoss(geneFamilyId = 10000, analysis = 2, cutoff = 0.95):
         oS = oS.reset_index()
         #which homologous IES columns have a change of probability presence larger than the cuttoff
         probabilityChange = oS.presence-aS.presence
-        losses = probabilityChange < - cutoff
-        gains = probabilityChange > cutoff
-        if losses.any() or gains.any():
-            if not aS.iesColumn.equals(oS.iesColumn):
-                print("Error! IES column numbers should match!")
-            for i, dp in enumerate(probabilityChange.tolist()):
-                event = None
-                if losses[i].item() == True:
-                    event = "loss"
-                elif gains[i].item() == True:
-                    event = "gain"
+        classIES = None
+        i = 0
+        for pAncestral, pOffspring in zip(oS.presence.tolist(), aS.presence.tolist()):
+            if pOffspring <= 0.01:
+                if pAncestral > 0.95:
+                    classIES = "lost"
+                elif pAncestral < 0.05:
+                    classIES = "absent"
+                elif pAncestral > 0.5:
+                    classIES = "probably_lost"
                 else:
-                    event = "undetermined"
-                with open(outputFile, 'a') as f:
-                    f.write('\t'.join([oS.cluster[i], oS.iesColumn[i], leaf.name, event + "\n"]))
-            #homIES['geneFamily' == geneFamilyId]
-            #geneFamilyId: oS.cluster[lost]
-            #iesColumn: oS.iesColumn[lost]
-            #node: leaf.name
+                    classIES = "probably_absent"
+            elif pOffspring >= 0.99:
+                if pAncestral > 0.95:
+                    classIES = "conserved"
+                elif pAncestral < 0.05:
+                    classIES = "gained"
+                elif pAncestral < 0.50:
+                    classIES = "probably_gained"
+                else:
+                    classIES = "probably_conserved"
+            else:
+                classIES = "undetermined"
+            with open(outputFile, 'a') as f:
+                f.write('\t'.join([oS.cluster[i], oS.iesColumn[i], leaf.name, classIES + "\n"]))
+            i += 1
 
 if geneFamilyId == None:
     #Run the analysis for all gene families for which we have ancestral state reconstructions
     for geneFamilyId in nodeProb.cluster.unique():
-        printRecentIESLoss(geneFamilyId, analysis, cutoff)
+        printRecentIESLoss(geneFamilyId, analysis)
 else:
-    printRecentIESLoss(geneFamilyId, analysis, cutoff)
+    printRecentIESLoss(geneFamilyId, analysis)
